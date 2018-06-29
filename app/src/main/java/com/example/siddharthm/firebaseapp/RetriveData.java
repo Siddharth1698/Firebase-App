7f2606ca6ade3add4ae187f95555bf8f89268780
Siddharth M <siddharth.m98@gmail.com>
2018-06-30 03:17:48 +0530

    Retrive data into a ListView

--- app/src/main/java/com/example/siddharthm/firebaseapp/RetriveData.java
+++ app/src/main/java/com/example/siddharthm/firebaseapp/RetriveData.java
@@ -1,10 +1,15 @@
 package com.example.siddharthm.firebaseapp;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -13,30 +18,48 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class RetriveData extends AppCompatActivity {
-    private TextView mValueView;
+    private ListView listView;
+    private ArrayList<String> mUsernames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fir-application-2c0e4.firebaseio.com/sidd");
         setContentView(R.layout.activity_retrive_data);
-        mValueView = (TextView)findViewById(R.id.valueView);
-        DatabaseReference mRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://fir-application-2c0e4.firebaseio.com/phill/sport");
+        listView = (ListView) findViewById(R.id.listView);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mUsernames);
+        listView.setAdapter(arrayAdapter);
 
-
-        mRef.addValueEventListener(new ValueEventListener() {
+        mRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 String value = dataSnapshot.getValue(String.class);
-                mValueView.setText(value);
+                mUsernames.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
             }
 
             @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
