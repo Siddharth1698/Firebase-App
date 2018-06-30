3f9f72bca63aa3cb5c410ab0fc3e27c40515b5e2
Siddharth M <siddharth.m98@gmail.com>
2018-06-30 17:34:43 +0530

    Uploading Image to dB

--- app/src/main/java/com/example/siddharthm/firebaseapp/ImageUpload.java
+++ app/src/main/java/com/example/siddharthm/firebaseapp/ImageUpload.java
@@ -1,24 +1,36 @@
 package com.example.siddharthm.firebaseapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.flags.IFlagProvider;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.net.URI;
 
 public class ImageUpload extends AppCompatActivity {
     Button imageSelect;
     private StorageReference mStorage;
     private static final int GALLERY_INTENT = 2;
+    private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_upload);
         imageSelect = (Button)findViewById(R.id.button12);
+        pd = new ProgressDialog(this);
         mStorage = FirebaseStorage.getInstance().getReference();
         imageSelect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +39,29 @@ public class ImageUpload extends AppCompatActivity {
                 intent.setType("image/*");
                 startActivityForResult(intent,GALLERY_INTENT);
             }
+
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+            pd.setMessage("Uploading");
+            pd.show();
+
+            Uri uri = data.getData();
+            StorageReference filepath = mStorage.child("Photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(ImageUpload.this,"Upload done",Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }
+            });
+
+
+        }
     }
 }
