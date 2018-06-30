99cdf64e7a23383de4cfa02e27b8d07768e66a74
Siddharth M <siddharth.m98@gmail.com>
2018-06-30 15:28:04 +0530

    Selecting Image from Gallery

---
+++ app/src/main/java/com/example/siddharthm/firebaseapp/ImageUpload.java
@@ -0,0 +1,33 @@
+package com.example.siddharthm.firebaseapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ImageUpload extends AppCompatActivity {
+    Button imageSelect;
+    private StorageReference mStorage;
+    private static final int GALLERY_INTENT = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_upload);
+        imageSelect = (Button)findViewById(R.id.button12);
+        mStorage = FirebaseStorage.getInstance().getReference();
+        imageSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,GALLERY_INTENT);
+            }
+        });
+
+    }
+}
